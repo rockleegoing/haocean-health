59bbe522b42bafa33f0b153df23acc1d11aa2b1a
arthur_lee <arthur.going@hotmail.com>
2026-04-25 02:29:09 +0800

    feat(android): WorkFragment改造为日常办公添加单位入口

--- Ruoyi-Android-App/app/src/main/java/com/ruoyi/app/fragment/WorkFragment.kt
+++ Ruoyi-Android-App/app/src/main/java/com/ruoyi/app/fragment/WorkFragment.kt
@@ -1,14 +1,8 @@
 package com.ruoyi.app.fragment
 
-import com.ruoyi.app.adapter.BannerHolderCreator
-import com.ruoyi.app.adapter.WorkManageAdapter
 import com.ruoyi.app.databinding.FragmentWorkBinding
 import com.ruoyi.app.model.Constant
-import com.ruoyi.app.model.UserViewModel
-import com.ruoyi.app.model.entity.BasicEntity
 import com.ruoyi.code.base.BaseBindingFragment
-import com.ruoyi.code.base.activityViewModels
-import com.ruoyi.code.witget.toast
 import com.therouter.router.Route
 
 @Route(path = Constant.workFragment)
@@ -19,34 +13,14 @@ class WorkFragment : BaseBindingFragment<FragmentWorkBinding>() {
         fun newInstance() = WorkFragment()
     }
 
-    private val adapter: WorkManageAdapter by lazy {
-        WorkManageAdapter()
-    }
-
-    private val viewModel: UserViewModel by activityViewModels()
-
     override fun initView() {
-        binding.recyclerview.adapter = adapter
+        binding.cardAddUnit.setOnClickListener {
+            TheRouter.build(Constant.addUnitRoute).navigation()
+        }
     }
 
     override fun initData() {
-        viewModel.getWorkData(requireActivity())
-        viewModel.errorMsg.observe(this) {
-            toast(it)
-        }
-        val holderCreator = BannerHolderCreator()
-        viewModel.workData.observe(this) {
-            val data = it.data
-            val banner = data.banner
-            binding.xbanner.setBannerData(banner, holderCreator)
-
-            val basic = data.function.basic
-            val plug = data.function.plug
-            val mList = ArrayList<BasicEntity>()
-            mList.add(basic)
-            mList.add(plug)
-            adapter.setNewInstance(mList)
-        }
+        // No data loading needed for this simple entry page
     }
 
 }
