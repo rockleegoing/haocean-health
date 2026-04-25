77ea5b9502f7f734b6e75f544f186ffce785ca47
arthur_lee <arthur.going@hotmail.com>
2026-04-25 13:16:23 +0800

    feat(service): 添加文书模块Service
    - 创建 ISysDocumentService 接口，定义文书模板、变量、套组、记录的CRUD方法
    - 创建 SysDocumentServiceImpl 实现类，注入对应Mapper并实现所有接口方法

---
+++ RuoYi-Vue/ruoyi-system/src/main/java/com/ruoyi/system/service/ISysDocumentService.java
@@ -0,0 +1,205 @@
+package com.ruoyi.system.service;
+
+import java.util.List;
+import com.ruoyi.system.domain.SysDocumentTemplate;
+import com.ruoyi.system.domain.SysDocumentVariable;
+import com.ruoyi.system.domain.SysDocumentGroup;
+import com.ruoyi.system.domain.SysDocumentRecord;
+
+/**
+ * 文书模块Service接口
+ *
+ * @author ruoyi
+ * @date 2026-04-25
+ */
+public interface ISysDocumentService {
+    // 文书模板
+
+    /**
+     * 查询文书模板
+     *
+     * @param id 文书模板主键
+     * @return 文书模板
+     */
+    SysDocumentTemplate selectSysDocumentTemplateById(Long id);
+
+    /**
+     * 查询文书模板列表
+     *
+     * @param template 文书模板
+     * @return 文书模板集合
+     */
+    List<SysDocumentTemplate> selectSysDocumentTemplateList(SysDocumentTemplate template);
+
+    /**
+     * 查询所有启用的模板
+     *
+     * @return 启用的模板列表
+     */
+    List<SysDocumentTemplate> selectAllSysDocumentTemplates();
+
+    /**
+     * 新增文书模板
+     *
+     * @param template 文书模板
+     * @return 结果
+     */
+    int insertSysDocumentTemplate(SysDocumentTemplate template);
+
+    /**
+     * 修改文书模板
+     *
+     * @param template 文书模板
+     * @return 结果
+     */
+    int updateSysDocumentTemplate(SysDocumentTemplate template);
+
+    /**
+     * 删除文书模板
+     *
+     * @param id 文书模板主键
+     * @return 结果
+     */
+    int deleteSysDocumentTemplateById(Long id);
+
+    /**
+     * 批量删除文书模板
+     *
+     * @param ids 需要删除的数据主键集合
+     * @return 结果
+     */
+    int deleteSysDocumentTemplateByIds(Long[] ids);
+
+    // 文书模板变量
+
+    /**
+     * 根据模板ID查询变量
+     *
+     * @param templateId 模板ID
+     * @return 文书模板变量列表
+     */
+    List<SysDocumentVariable> selectVariablesByTemplateId(Long templateId);
+
+    /**
+     * 新增文书模板变量
+     *
+     * @param variable 文书模板变量
+     * @return 结果
+     */
+    int insertSysDocumentVariable(SysDocumentVariable variable);
+
+    /**
+     * 修改文书模板变量
+     *
+     * @param variable 文书模板变量
+     * @return 结果
+     */
+    int updateSysDocumentVariable(SysDocumentVariable variable);
+
+    /**
+     * 删除文书模板变量
+     *
+     * @param id 文书模板变量主键
+     * @return 结果
+     */
+    int deleteSysDocumentVariableById(Long id);
+
+    // 文书套组
+
+    /**
+     * 查询文书套组
+     *
+     * @param id 文书套组主键
+     * @return 文书套组
+     */
+    SysDocumentGroup selectSysDocumentGroupById(Long id);
+
+    /**
+     * 查询文书套组列表
+     *
+     * @param group 文书套组
+     * @return 文书套组集合
+     */
+    List<SysDocumentGroup> selectSysDocumentGroupList(SysDocumentGroup group);
+
+    /**
+     * 查询所有启用的套组
+     *
+     * @return 启用的套组列表
+     */
+    List<SysDocumentGroup> selectAllSysDocumentGroups();
+
+    /**
+     * 新增文书套组
+     *
+     * @param group 文书套组
+     * @return 结果
+     */
+    int insertSysDocumentGroup(SysDocumentGroup group);
+
+    /**
+     * 修改文书套组
+     *
+     * @param group 文书套组
+     * @return 结果
+     */
+    int updateSysDocumentGroup(SysDocumentGroup group);
+
+    /**
+     * 删除文书套组
+     *
+     * @param id 文书套组主键
+     * @return 结果
+     */
+    int deleteSysDocumentGroupById(Long id);
+
+    // 文书记录
+
+    /**
+     * 查询文书记录
+     *
+     * @param id 文书记录主键
+     * @return 文书记录
+     */
+    SysDocumentRecord selectSysDocumentRecordById(Long id);
+
+    /**
+     * 查询文书记录列表
+     *
+     * @param record 文书记录
+     * @return 文书记录集合
+     */
+    List<SysDocumentRecord> selectSysDocumentRecordList(SysDocumentRecord record);
+
+    /**
+     * 根据执法记录ID查询文书列表
+     *
+     * @param enforcementRecordId 执法记录ID
+     * @return 文书记录列表
+     */
+    List<SysDocumentRecord> selectRecordsByEnforcementRecordId(Long enforcementRecordId);
+
+    /**
+     * 新增文书记录
+     *
+     * @param record 文书记录
+     * @return 结果
+     */
+    int insertSysDocumentRecord(SysDocumentRecord record);
+
+    /**
+     * 修改文书记录
+     *
+     * @param record 文书记录
+     * @return 结果
+     */
+    int updateSysDocumentRecord(SysDocumentRecord record);
+
+    /**
+     * 删除文书记录
+     *
+     * @param id 文书记录主键
+     * @return 结果
+     */
+    int deleteSysDocumentRecordById(Long id);
+}
---
+++ RuoYi-Vue/ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SysDocumentServiceImpl.java
@@ -0,0 +1,246 @@
+package com.ruoyi.system.service.impl;
+
+import java.util.List;
+import com.ruoyi.common.utils.DateUtils;
+import com.ruoyi.common.utils.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.ruoyi.system.mapper.SysDocumentTemplateMapper;
+import com.ruoyi.system.mapper.SysDocumentVariableMapper;
+import com.ruoyi.system.mapper.SysDocumentGroupMapper;
+import com.ruoyi.system.mapper.SysDocumentRecordMapper;
+import com.ruoyi.system.domain.SysDocumentTemplate;
+import com.ruoyi.system.domain.SysDocumentVariable;
+import com.ruoyi.system.domain.SysDocumentGroup;
+import com.ruoyi.system.domain.SysDocumentRecord;
+import com.ruoyi.system.service.ISysDocumentService;
+
+/**
+ * 文书模块Service业务层处理
+ *
+ * @author ruoyi
+ * @date 2026-04-25
+ */
+@Service
+public class SysDocumentServiceImpl implements ISysDocumentService {
+    @Autowired
+    private SysDocumentTemplateMapper sysDocumentTemplateMapper;
+
+    @Autowired
+    private SysDocumentVariableMapper sysDocumentVariableMapper;
+
+    @Autowired
+    private SysDocumentGroupMapper sysDocumentGroupMapper;
+
+    @Autowired
+    private SysDocumentRecordMapper sysDocumentRecordMapper;
+
+    // ==================== 文书模板 ====================
+
+    /**
+     * 查询文书模板
+     */
+    @Override
+    public SysDocumentTemplate selectSysDocumentTemplateById(Long id) {
+        return sysDocumentTemplateMapper.selectSysDocumentTemplateById(id);
+    }
+
+    /**
+     * 查询文书模板列表
+     */
+    @Override
+    public List<SysDocumentTemplate> selectSysDocumentTemplateList(SysDocumentTemplate template) {
+        return sysDocumentTemplateMapper.selectSysDocumentTemplateList(template);
+    }
+
+    /**
+     * 查询所有启用的模板
+     */
+    @Override
+    public List<SysDocumentTemplate> selectAllSysDocumentTemplates() {
+        return sysDocumentTemplateMapper.selectAllSysDocumentTemplates();
+    }
+
+    /**
+     * 新增文书模板
+     */
+    @Override
+    public int insertSysDocumentTemplate(SysDocumentTemplate template) {
+        template.setCreateTime(DateUtils.getNowDate());
+        template.setCreateBy(SecurityUtils.getUsername());
+        return sysDocumentTemplateMapper.insertSysDocumentTemplate(template);
+    }
+
+    /**
+     * 修改文书模板
+     */
+    @Override
+    public int updateSysDocumentTemplate(SysDocumentTemplate template) {
+        template.setUpdateTime(DateUtils.getNowDate());
+        template.setUpdateBy(SecurityUtils.getUsername());
+        return sysDocumentTemplateMapper.updateSysDocumentTemplate(template);
+    }
+
+    /**
+     * 删除文书模板
+     */
+    @Override
+    public int deleteSysDocumentTemplateById(Long id) {
+        return sysDocumentTemplateMapper.deleteSysDocumentTemplateById(id);
+    }
+
+    /**
+     * 批量删除文书模板
+     */
+    @Override
+    public int deleteSysDocumentTemplateByIds(Long[] ids) {
+        return sysDocumentTemplateMapper.deleteSysDocumentTemplateByIds(ids);
+    }
+
+    // ==================== 文书模板变量 ====================
+
+    /**
+     * 根据模板ID查询变量
+     */
+    @Override
+    public List<SysDocumentVariable> selectVariablesByTemplateId(Long templateId) {
+        return sysDocumentVariableMapper.selectVariablesByTemplateId(templateId);
+    }
+
+    /**
+     * 新增文书模板变量
+     */
+    @Override
+    public int insertSysDocumentVariable(SysDocumentVariable variable) {
+        variable.setCreateTime(DateUtils.getNowDate());
+        variable.setCreateBy(SecurityUtils.getUsername());
+        return sysDocumentVariableMapper.insertSysDocumentVariable(variable);
+    }
+
+    /**
+     * 修改文书模板变量
+     */
+    @Override
+    public int updateSysDocumentVariable(SysDocumentVariable variable) {
+        variable.setUpdateTime(DateUtils.getNowDate());
+        variable.setUpdateBy(SecurityUtils.getUsername());
+        return sysDocumentVariableMapper.updateSysDocumentVariable(variable);
+    }
+
+    /**
+     * 删除文书模板变量
+     */
+    @Override
+    public int deleteSysDocumentVariableById(Long id) {
+        return sysDocumentVariableMapper.deleteSysDocumentVariableById(id);
+    }
+
+    // ==================== 文书套组 ====================
+
+    /**
+     * 查询文书套组
+     */
+    @Override
+    public SysDocumentGroup selectSysDocumentGroupById(Long id) {
+        return sysDocumentGroupMapper.selectSysDocumentGroupById(id);
+    }
+
+    /**
+     * 查询文书套组列表
+     */
+    @Override
+    public List<SysDocumentGroup> selectSysDocumentGroupList(SysDocumentGroup group) {
+        return sysDocumentGroupMapper.selectSysDocumentGroupList(group);
+    }
+
+    /**
+     * 查询所有启用的套组
+     */
+    @Override
+    public List<SysDocumentGroup> selectAllSysDocumentGroups() {
+        return sysDocumentGroupMapper.selectAllSysDocumentGroups();
+    }
+
+    /**
+     * 新增文书套组
+     */
+    @Override
+    public int insertSysDocumentGroup(SysDocumentGroup group) {
+        group.setCreateTime(DateUtils.getNowDate());
+        group.setCreateBy(SecurityUtils.getUsername());
+        return sysDocumentGroupMapper.insertSysDocumentGroup(group);
+    }
+
+    /**
+     * 修改文书套组
+     */
+    @Override
+    public int updateSysDocumentGroup(SysDocumentGroup group) {
+        group.setUpdateTime(DateUtils.getNowDate());
+        group.setUpdateBy(SecurityUtils.getUsername());
+        return sysDocumentGroupMapper.updateSysDocumentGroup(group);
+    }
+
+    /**
+     * 删除文书套组
+     */
+    @Override
+    public int deleteSysDocumentGroupById(Long id) {
+        return sysDocumentGroupMapper.deleteSysDocumentGroupById(id);
+    }
+
+    // ==================== 文书记录 ====================
+
+    /**
+     * 查询文书记录
+     */
+    @Override
+    public SysDocumentRecord selectSysDocumentRecordById(Long id) {
+        return sysDocumentRecordMapper.selectSysDocumentRecordById(id);
+    }
+
+    /**
+     * 查询文书记录列表
+     */
+    @Override
+    public List<SysDocumentRecord> selectSysDocumentRecordList(SysDocumentRecord record) {
+        return sysDocumentRecordMapper.selectSysDocumentRecordList(record);
+    }
+
+    /**
+     * 根据执法记录ID查询文书列表
+     */
+    @Override
+    public List<SysDocumentRecord> selectRecordsByEnforcementRecordId(Long enforcementRecordId) {
+        return sysDocumentRecordMapper.selectRecordsByEnforcementRecordId(enforcementRecordId);
+    }
+
+    /**
+     * 新增文书记录
+     */
+    @Override
+    public int insertSysDocumentRecord(SysDocumentRecord record) {
+        record.setCreateTime(DateUtils.getNowDate());
+        record.setCreateBy(SecurityUtils.getUsername());
+        return sysDocumentRecordMapper.insertSysDocumentRecord(record);
+    }
+
+    /**
+     * 修改文书记录
+     */
+    @Override
+    public int updateSysDocumentRecord(SysDocumentRecord record) {
+        record.setUpdateTime(DateUtils.getNowDate());
+        record.setUpdateBy(SecurityUtils.getUsername());
+        return sysDocumentRecordMapper.updateSysDocumentRecord(record);
+    }
+
+    /**
+     * 删除文书记录
+     */
+    @Override
+    public int deleteSysDocumentRecordById(Long id) {
+        return sysDocumentRecordMapper.deleteSysDocumentRecordById(id);
+    }
+}
