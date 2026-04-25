6da0967439839f4ed648b0a948f1c6f0574f1c95
arthur_lee <arthur.going@hotmail.com>
2026-04-25 13:42:31 +0800

    feat(frontend): 添加文书管理主页面索引
    创建文书管理主页面，支持模板管理和套组管理切换

---
+++ RuoYi-Vue/ruoyi-ui/src/views/system/document/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app-container">
+    <el-tabs v-model="activeName" @tab-click="handleTabClick">
+      <el-tab-pane label="模板管理" name="template">
+        <template v-if="activeName === 'template'">
+          <template-list />
+        </template>
+      </el-tab-pane>
+      <el-tab-pane label="套组管理" name="group">
+        <template v-if="activeName === 'group'">
+          <group-list />
+        </template>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import TemplateList from './template/index.vue'
+import GroupList from './group/index.vue'
+
+export default {
+  name: 'Document',
+  components: { TemplateList, GroupList },
+  data() {
+    return {
+      activeName: 'template'
+    }
+  },
+  methods: {
+    handleTabClick(tab) {
+      this.activeName = tab.name
+    }
+  }
+}
+</script>
