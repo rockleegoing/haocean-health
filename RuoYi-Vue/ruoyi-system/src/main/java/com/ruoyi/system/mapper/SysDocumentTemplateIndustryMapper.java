d1ce52749c5ca620b6e030e2eaec66fe7c72254c
arthur_lee <arthur.going@hotmail.com>
2026-04-26 17:55:01 +0800

    feat(document): 添加文书模板行业关联Mapper接口

---
+++ RuoYi-Vue/ruoyi-system/src/main/java/com/ruoyi/system/mapper/SysDocumentTemplateIndustryMapper.java
@@ -0,0 +1,22 @@
+package com.ruoyi.system.mapper;
+
+import com.ruoyi.system.domain.SysDocumentTemplateIndustry;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
+
+/**
+ * 文书模板与行业分类关联Mapper接口
+ *
+ * @author ruoyi
+ * @date 2026-04-26
+ */
+public interface SysDocumentTemplateIndustryMapper {
+
+    int insertBatch(@Param("list") List<SysDocumentTemplateIndustry> list);
+
+    int deleteByTemplateId(Long templateId);
+
+    List<Long> selectByIndustryCategoryId(Long industryCategoryId);
+
+    List<SysDocumentTemplateIndustry> selectAll();
+}
