97490a71bb7e87e0884c6df1f468e54e9f9ea5f1
arthur_lee <arthur.going@hotmail.com>
2026-04-28 11:42:29 +0800

    feat(system): 添加定性依据内容表实体，精简主表字段

--- RuoYi-Vue/ruoyi-system/src/main/java/com/ruoyi/system/domain/SysLegalBasis.java
+++ RuoYi-Vue/ruoyi-system/src/main/java/com/ruoyi/system/domain/SysLegalBasis.java
@@ -17,38 +17,9 @@ public class SysLegalBasis extends BaseEntity {
     /** 定性依据ID */
     private Long basisId;
 
-    /** 编号（如：001） */
-    @Excel(name = "编号")
-    private String basisNo;
-
     /** 标题 */
     @Excel(name = "标题")
     private String title;
-
-    /** 违法类型 */
-    @Excel(name = "违法类型")
-    private String violationType;
-
-    /** 颁发机构 */
-    @Excel(name = "颁发机构")
-    private String issuingAuthority;
-
-    /** 实施时间 */
-    @Excel(name = "实施时间")
-    private String effectiveDate;
-
-    /** 效级 */
-    @Excel(name = "效级")
-    private String legalLevel;
-
-    /** 条款内容（JSON数组） */
-    private String clauses;
-
-    /** 法律责任 */
-    private String legalLiability;
-
-    /** 裁量标准 */
-    private String discretionStandard;
 
     /** 关联法律法规ID */
     private Long regulationId;
@@ -71,76 +42,12 @@ public class SysLegalBasis extends BaseEntity {
         return basisId;
     }
 
-    public void setBasisNo(String basisNo) {
-        this.basisNo = basisNo;
-    }
-
-    public String getBasisNo() {
-        return basisNo;
-    }
-
     public void setTitle(String title) {
         this.title = title;
     }
 
     public String getTitle() {
         return title;
-    }
-
-    public void setViolationType(String violationType) {
-        this.violationType = violationType;
-    }
-
-    public String getViolationType() {
-        return violationType;
-    }
-
-    public void setIssuingAuthority(String issuingAuthority) {
-        this.issuingAuthority = issuingAuthority;
-    }
-
-    public String getIssuingAuthority() {
-        return issuingAuthority;
-    }
-
-    public void setEffectiveDate(String effectiveDate) {
-        this.effectiveDate = effectiveDate;
-    }
-
-    public String getEffectiveDate() {
-        return effectiveDate;
-    }
-
-    public void setLegalLevel(String legalLevel) {
-        this.legalLevel = legalLevel;
-    }
-
-    public String getLegalLevel() {
-        return legalLevel;
-    }
-
-    public void setClauses(String clauses) {
-        this.clauses = clauses;
-    }
-
-    public String getClauses() {
-        return clauses;
-    }
-
-    public void setLegalLiability(String legalLiability) {
-        this.legalLiability = legalLiability;
-    }
-
-    public String getLegalLiability() {
-        return legalLiability;
-    }
-
-    public void setDiscretionStandard(String discretionStandard) {
-        this.discretionStandard = discretionStandard;
-    }
-
-    public String getDiscretionStandard() {
-        return discretionStandard;
     }
 
     public void setRegulationId(Long regulationId) {
@@ -179,15 +86,7 @@ public class SysLegalBasis extends BaseEntity {
     public String toString() {
         return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
                 .append("basisId", getBasisId())
-                .append("basisNo", getBasisNo())
                 .append("title", getTitle())
-                .append("violationType", getViolationType())
-                .append("issuingAuthority", getIssuingAuthority())
-                .append("effectiveDate", getEffectiveDate())
-                .append("legalLevel", getLegalLevel())
-                .append("clauses", getClauses())
-                .append("legalLiability", getLegalLiability())
-                .append("discretionStandard", getDiscretionStandard())
                 .append("regulationId", getRegulationId())
                 .append("status", getStatus())
                 .append("delFlag", getDelFlag())
