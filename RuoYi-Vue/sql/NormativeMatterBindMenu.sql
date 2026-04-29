028a5507cd898a13b174ebf7131297ae82e23124
arthur_lee <arthur.going@hotmail.com>
2026-04-29 19:19:41 +0800

    docs: 简化菜单项名称
    将"规范用语法律条款关联"改为"规范法律关联"
    将"规范用语监管事项关联"改为"规范监管关联"

--- RuoYi-Vue/sql/NormativeMatterBindMenu.sql
+++ RuoYi-Vue/sql/NormativeMatterBindMenu.sql
@@ -1,6 +1,6 @@
 -- 菜单 SQL
 insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
-values('规范用语监管事项关联', '1224', '1', 'normativematterbind', 'system/normativematterbind/index', 1, 0, 'C', '0', '0', 'system:normativematterbind:list', '#', 'admin', sysdate(), '', null, '规范用语监管事项关联菜单');
+values('规范监管关联', '1224', '1', 'normativematterbind', 'system/normativematterbind/index', 1, 0, 'C', '0', '0', 'system:normativematterbind:list', '#', 'admin', sysdate(), '', null, '规范用语监管事项关联菜单');
 
 -- 按钮父菜单ID
 SELECT @parentId := LAST_INSERT_ID();
